d155e0c727ca303b150943e7644036e50ad28eef
arlendp <dupeng1160058696@qq.com>
2017-12-13 20:20:50 +0800

    edit method name

--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
 import ScrollBar from './scrollbar';
 export default function initScrollBar(options) {
   const scrollbar = new ScrollBar(options);
-  scrollbar.generateElems();
-  scrollbar.bindMouseEvent();
+  scrollbar._generateElems();
+  scrollbar._bindMouseEvent();
   return scrollbar;
 }
